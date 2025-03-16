6973cf75e5bcf3cd1000b1d30b8ecca5755642e7
Haruna Nishita <h.nishita.1@gmail.com>
2025-03-16 15:25:59 +0900

    add: TODOの作成用メソッドとエンドポイントを追加

---
+++ schemas.py
@@ -0,0 +1,14 @@
+from pydantic import BaseModel
+
+class Todo(BaseModel):
+  id: str
+  title: str
+  description: str
+
+
+class TodoBody(BaseModel):
+  title: str
+  description: str
+  
+class SuccessMessage(BaseModel):
+  message: str
