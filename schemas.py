baee87ed17d78b70b0c7c33e62be525caaadff78
Haruna Nishita <h.nishita.1@gmail.com>
2025-03-20 19:10:13 +0900

    add: ユーザーの作成とログイン機能の追加

--- schemas.py
+++ schemas.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from pydantic import BaseModel
 
 class Todo(BaseModel):
@@ -12,3 +13,12 @@ class TodoBody(BaseModel):
   
 class SuccessMessage(BaseModel):
   message: str
+
+
+class UserBody(BaseModel):
+  email: str
+  password: str
+
+class UserInfo(BaseModel):
+  id: Optional[str] = None
+  email: str
